1f29906346979f4091ee945709a06f14c65b4055
ItsMuri <muresan.andrei84@gmail.com>
2018-10-22 12:43:54 +0200

    Test commit

---
+++ Client/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            AsyncMain().Wait();
+        }
+
+        static async Task AsyncMain()
+        {
+            TcpClient client = new TcpClient(new IPEndPoint(IPAddress.Loopback, 0));
+            await client.ConnectAsync(IPAddress.Loopback, 5566);
+
+            using (NetworkStream stream = client.GetStream())
+            {
+                while (true)
+                {
+                    using (var reader = new StreamReader(stream, Encoding.ASCII, true, 4096, leaveOpen: true))
+                    {
+                        int threadid = Thread.CurrentThread.ManagedThreadId;
+                        string answer = reader.ReadLine();
+                        Console.WriteLine(answer);
+                        Console.WriteLine($"Current thread {threadid}: Calculating...");
+                        Thread.Sleep(2000);
+                        
+                        Console.WriteLine($"Current thread {threadid}: Finished");
+                    }
+
+                    using (var writer = new StreamWriter(stream, Encoding.ASCII, 4096, leaveOpen: true))
+                    {
+                        string msg = "Client 1 finished calculation!";
+                        writer.WriteLine(msg);
+                    }
+                }
+            }
+        }
+    }
+}
--- Server/MainWindow.xaml.cs
+++ Server/MainWindow.xaml.cs
@@ -1,7 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Net;
+using System.Net.Sockets;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -20,9 +24,52 @@ namespace Server
     /// </summary>
     public partial class MainWindow : Window
     {
+        private TcpListener listener;
+
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void Button_Loaded(object sender, RoutedEventArgs e)
+        {
+            Task.Run(() =>
+            {
+                listener = new TcpListener(IPAddress.Loopback, 5566);
+                listener.Start();
+            });
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Task t = CalculateTask();
+        }
+
+        private async Task CalculateTask()
+        {
+            while (true)
+            {
+                TcpClient tcpClient = await listener.AcceptTcpClientAsync();
+
+                using (NetworkStream stream = tcpClient.GetStream())
+                {
+                    while (true)
+                    {
+                        using (var writer = new StreamWriter(stream, Encoding.ASCII, 4096, leaveOpen: true))
+                        {
+                            string str = Convert.ToString("Connection established");
+                            writer.WriteLine(str);
+                        }
+
+                        using (var reader =
+                            new StreamReader(stream, Encoding.ASCII, true, 4096, leaveOpen: true))
+                        {
+                            string answer = reader.ReadLine();
+                            MessageBox.Show($"{answer}");
+                        }
+                    }
+                }
+            }
         }
     }
 }
