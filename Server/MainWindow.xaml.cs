174eebc4f023adafba45afd5ab3eab2b7ee4bea8
LandMar <mario.landekic@gmail.com>
2019-01-22 09:40:56 +0100

    Zoom Funktion hinzugefügt

--- Server/MainWindow.xaml.cs
+++ Server/MainWindow.xaml.cs
@@ -1,46 +1,48 @@
 ﻿using System;
-using System.Collections;
 using System.Collections.Generic;
 using System.Drawing;
+using System.Drawing.Imaging;
 using System.IO;
 using System.Linq;
 using System.Net;
 using System.Net.Sockets;
 using System.Runtime.Serialization;
-using System.Text;
-using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
-using System.Windows.Media.Effects;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 using SerializedFraktal;
 using Color = System.Drawing.Color;
+using Point = System.Windows.Point;
 
 namespace Server
 {
     /// <summary>
-    /// Interaktionslogik für MainWindow.xaml
+    ///     Interaktionslogik für MainWindow.xaml
     /// </summary>
     public partial class MainWindow : Window
     {
+        private readonly List<Bitmap> bitmapList = new List<Bitmap>();
+        private readonly List<TcpClient> listConnectedClients = new List<TcpClient>();
         private TcpListener listener;
-        List<Bitmap> bitmapList = new List<Bitmap>();
-        List<TcpClient> listConnectedClients = new List<TcpClient>();
+        private Point origin;
+
+        private Point start;
+        //TransformGroup group = new TransformGroup();
+        //ScaleTransform st = new ScaleTransform();
+        //TranslateTransform tt = new TranslateTransform();
 
 
         public MainWindow()
         {
             InitializeComponent();
 
-            //Zeichnen lassen
-            //Vielleicht kann man das mit WriteableBitmap zeichnen lassen...
+            BorderImage.ClipToBounds = true;
+
+            //group.Children.Add(st);
+            //group.Children.Add(tt);
+            //imageFraktal.RenderTransform = group;
         }
 
         private void ZeichneFraktal()
@@ -49,68 +51,62 @@ namespace Server
             //Referenz auf Video YT in liked Liste!
 
 
-            int cntInterations = Convert.ToInt32(Dispatcher.Invoke(() => tbIterations.Text));
-            FraktalTask ft = new FraktalTask();
+            var cntInterations = Convert.ToInt32(Dispatcher.Invoke(() => tbIterations.Text));
+            var ft = new FraktalTask();
             // in ft werden die verschiedenen Koordinaten 
 
 
             //Versuche das Fraktal in 2 Sektoren zu unterteilen!!!
-            Bitmap bm = new Bitmap(Convert.ToInt32(imageFraktal.Width), Convert.ToInt32(imageFraktal.Height));
-            Bitmap lowerBm = new Bitmap(Convert.ToInt32(imageFraktal.Width / 2),
+            var bm = new Bitmap(Convert.ToInt32(imageFraktal.Width), Convert.ToInt32(imageFraktal.Height));
+            var lowerBm = new Bitmap(Convert.ToInt32(imageFraktal.Width / 2),
                 Convert.ToInt32(imageFraktal.Height / 2));
             //Haben jetzt zwei Bereiche, lowerBm ist der untere Bereich der Bitmap
             //also in diesem Fall 200 x 200 bei einem Original von 400 x 400
             //Idee: Färbe zuerst die 400 x 400 Fläche und danach die 200 x 200 Fläche
 
 
-            for (int x = 0; x < imageFraktal.Width / 2; x++)
-            {
-                for (int y = 0; y < imageFraktal.Height; y++)
+            for (var x = 0; x < imageFraktal.Width / 2; x++)
+                for (var y = 0; y < imageFraktal.Height; y++)
                 {
-                    double a = (double) (x - (imageFraktal.Width / 2)) / (double) (imageFraktal.Width / 4);
-                    double b = (double) (y - (imageFraktal.Height / 2)) / (double) (imageFraktal.Height / 4);
-                    Complex c = new Complex(a, b);
-                    Complex z = new Complex(0, 0);
-                    int it = 0;
+                    var a = (x - imageFraktal.Width / 2) / (imageFraktal.Width / 4);
+                    var b = (y - imageFraktal.Height / 2) / (imageFraktal.Height / 4);
+                    var c = new Complex(a, b);
+                    var z = new Complex(0, 0);
+                    var it = 0;
                     do
                     {
                         it++;
                         z.Square();
                         z.Add(c);
                         if (z.Magnitude() > 2.0)
-                        {
                             break;
-                        }
                     } while (it < cntInterations);
+
 
                     //bm.SetPixel(x, y, it < 50 ? Color.Black : Color.Blue);
                     bm.SetPixel(x, y, it < cntInterations ? Color.Aquamarine : Color.Red);
                     //lowerBm.SetPixel(x,y, it < cntInterations ? Color.Yellow : Color.Blue);
                     //Mehrere Farben so anzeigen lassen, funktioniert so nicht!!!
                 }
-            }
 
 
             imageFraktal.Source = BitmapToImageSource(bm);
 
-            for (double x = imageFraktal.Width / 2; x < imageFraktal.Width; x++)
-            {
-                for (int y = 0; y < imageFraktal.Height; y++)
+            for (var x = imageFraktal.Width / 2; x < imageFraktal.Width; x++)
+                for (var y = 0; y < imageFraktal.Height; y++)
                 {
-                    double a = (double) (x - (imageFraktal.Width / 2)) / (double) (imageFraktal.Width / 4);
-                    double b = (double) (y - (imageFraktal.Height / 2)) / (double) (imageFraktal.Height / 4);
-                    Complex c = new Complex(a, b);
-                    Complex z = new Complex(0, 0);
-                    int it = 0;
+                    var a = (x - imageFraktal.Width / 2) / (imageFraktal.Width / 4);
+                    var b = (y - imageFraktal.Height / 2) / (imageFraktal.Height / 4);
+                    var c = new Complex(a, b);
+                    var z = new Complex(0, 0);
+                    var it = 0;
                     do
                     {
                         it++;
                         z.Square();
                         z.Add(c);
                         if (z.Magnitude() > 2.0)
-                        {
                             break;
-                        }
                     } while (it < cntInterations);
 
                     //bm.SetPixel(x, y, it < 50 ? Color.Black : Color.Blue);
@@ -118,7 +114,6 @@ namespace Server
                     //lowerBm.SetPixel(x,y, it < cntInterations ? Color.Yellow : Color.Blue);
                     //Mehrere Farben so anzeigen lassen, funktioniert so nicht!!!
                 }
-            }
 
             imageFraktal.Source = BitmapToImageSource(bm);
         }
@@ -145,14 +140,14 @@ namespace Server
 
                 myFraktal.ID += 1;
 
-                NetworkStream netStream = mySender.GetStream();
+                var netStream = mySender.GetStream();
                 var serializer = new DataContractSerializer(typeof(PropsOfFractal));
                 serializer.WriteObject(netStream, myFraktal);
 
                 mySender.Client.Shutdown(SocketShutdown.Send);
 
                 var bitmSerializer = new DataContractSerializer(typeof(Bitmap));
-                var verarbeiteteDaten = (Bitmap) bitmSerializer.ReadObject(netStream);
+                var verarbeiteteDaten = (Bitmap)bitmSerializer.ReadObject(netStream);
 
                 netStream.Close();
                 mySender.Close();
@@ -181,24 +176,16 @@ namespace Server
         {
             //foreach (var item in bitmapList)
             //{
-            int width = Convert.ToInt32(myFraktal.imgWidth);
-            int height = Convert.ToInt32(myFraktal.imgHeight);
-            Bitmap newImage = new Bitmap(width, height);
+            var width = Convert.ToInt32(myFraktal.imgWidth);
+            var height = Convert.ToInt32(myFraktal.imgHeight);
+            var newImage = new Bitmap(width, height);
 
-            for (int x = 0; x < width / 2; x++)
-            {
-                for (int y = 0; y < height; y++)
-                {
+            for (var x = 0; x < width / 2; x++)
+                for (var y = 0; y < height; y++)
                     newImage.SetPixel(x, y, bitmapList[0].GetPixel(x, y));
-                }
-            }
-            for (int x = width / 2; x < width; x++)
-            {
-                for (int y = 0; y < height; y++)
-                {
+            for (var x = width / 2; x < width; x++)
+                for (var y = 0; y < height; y++)
                     newImage.SetPixel(x, y, bitmapList[1].GetPixel(x, y));
-                }
-            }
             //}
 
             //BitmapImage bmi = BitmapToImageSource(verabeiteteDaten);
@@ -220,8 +207,8 @@ namespace Server
             //ZeichneFraktal();
 
 
-            int iterationsCount = Convert.ToInt32(tbIterations.Text);
-            PropsOfFractal myFraktal = new PropsOfFractal(iterationsCount)
+            var iterationsCount = Convert.ToInt32(tbIterations.Text);
+            var myFraktal = new PropsOfFractal(iterationsCount)
             {
                 imgWidth = imageFraktal.Width,
                 imgHeight = imageFraktal.Height
@@ -241,7 +228,7 @@ namespace Server
             myFraktal.KoordinatenY = ycoordinates; //Y Koordinaten gesetzt!
             */
 
-            Task senden = new Task(() =>
+            var senden = new Task(() =>
             {
                 Senden(myFraktal);
                 //ZeichneFraktal();
@@ -301,13 +288,13 @@ namespace Server
             }*/
         }
 
-        BitmapImage BitmapToImageSource(Bitmap bitmap)
+        private BitmapImage BitmapToImageSource(Bitmap bitmap)
         {
-            using (MemoryStream memory = new MemoryStream())
+            using (var memory = new MemoryStream())
             {
-                bitmap.Save(memory, System.Drawing.Imaging.ImageFormat.Bmp);
+                bitmap.Save(memory, ImageFormat.Bmp);
                 memory.Position = 0;
-                BitmapImage bitmapimage = new BitmapImage();
+                var bitmapimage = new BitmapImage();
                 bitmapimage.BeginInit();
                 bitmapimage.StreamSource = memory;
                 bitmapimage.CacheOption = BitmapCacheOption.OnLoad;
@@ -315,6 +302,43 @@ namespace Server
 
                 return bitmapimage;
             }
+        }
+
+        private void ImageFraktal_MouseWheel(object sender, MouseWheelEventArgs e)
+        {
+            var st = (ScaleTransform)((TransformGroup)imageFraktal.RenderTransform).Children.First(tr =>
+              tr is ScaleTransform);
+            var zoom = e.Delta > 0 ? .2 : -.2;
+            st.ScaleX += zoom;
+            st.ScaleY += zoom;
+        }
+
+        private void ImageFraktal_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            imageFraktal.CaptureMouse();
+            var tt = (TranslateTransform)((TransformGroup)imageFraktal.RenderTransform).Children.First(tr =>
+              tr is TranslateTransform);
+
+            start = e.GetPosition(BorderImage);
+            origin = new Point(tt.X, tt.Y);
+        }
+
+        private void ImageFraktal_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (imageFraktal.IsMouseCaptured)
+            {
+                var tt = (TranslateTransform)((TransformGroup)imageFraktal.RenderTransform).Children.First(tr =>
+                  tr is TranslateTransform);
+
+                var v = start - e.GetPosition(BorderImage);
+                tt.X = origin.X - v.X;
+                tt.Y = origin.Y - v.Y;
+            }
+        }
+
+        private void ImageFraktal_MouseLeftButtonUp(object sender, MouseButtonEventArgs e)
+        {
+            imageFraktal.ReleaseMouseCapture();
         }
     }
 }
