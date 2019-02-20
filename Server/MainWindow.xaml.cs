4b34a0af4b3964c40c5490d2d16eb5d0a3ae3131
mojdehkar497j <mojdehkar497j@htl-ottakring.ac.at>
2019-02-20 10:59:35 +0100

    success() änderung

--- Server/MainWindow.xaml.cs
+++ Server/MainWindow.xaml.cs
@@ -106,21 +106,10 @@ namespace Server
         {
             while (true)
             {
-                try
+                while(Success() == false)
                 {
-
                     Success();
                 }
-                catch (Exception e)
-                {
-                    Thread.Sleep(5000);
-                    Success();
-                    if (Success() == true)
-                    {
-                        MessageBox.Show("Erfolgreiche Verbindung zw Server und Backup aufgenommen");
-                    }
-                }
-
             }
         }
 
